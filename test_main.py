dacbcbb33cf0fbf5fc0db90bb8a1810b1471384d
Franco Scaccheri <francocass2009@hotmail.com>
2021-10-03 23:27:38 -0300

    Test github actions

--- test_main.py
+++ test_main.py
@@ -6,5 +6,6 @@ client = TestClient(app)
 
 def test_home():
     response = client.get('/')
+    assert response.status_code != 400
     assert response.status_code == 200
     assert response.json() == {'message': 'Hello world!'}
