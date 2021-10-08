ab7de9845278cdaa06145b67d686b7fd4914e6e9
Franco Scaccheri <fcassanello@gitlab.com>
2021-10-07 21:26:51 -0300

    Remove authentication logic. Add endpoint to get User by username

---
+++ models/user.py
@@ -0,0 +1,21 @@
+from pydantic import BaseModel
+from typing import Optional
+
+
+fake_users_db = {
+    "johndoe": {
+        "username": "johndoe",
+        "full_name": "John Doe",
+        "email": "johndoe@example.com",
+        "hashed_password": "$2b$12$EixZaYVK1fsbw1ZfbX3OXePaWxn96p36WQoeG6Lruj3vjPGga31lW",
+        "disabled": False,
+    }
+}
+
+
+class User(BaseModel):
+    username: str
+    hashed_password: str
+    email: Optional[str] = None
+    full_name: Optional[str] = None
+    disabled: Optional[bool] = None
