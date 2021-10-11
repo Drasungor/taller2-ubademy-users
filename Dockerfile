1aaef294ee61bbf541123fe8a98da5f467ab9a6a
Agustin <aguscambiano@gmail.com>
2021-10-11 19:21:28 -0300

    Adapted program to run in a container

---
+++ Dockerfile
@@ -0,0 +1,23 @@
+FROM ubuntu
+
+WORKDIR /app
+RUN apt-get update
+#RUN apt-get -y install postgresql-client
+RUN apt-get -y update
+RUN apt-get -y install python3
+RUN apt-get -y install python3-pip
+RUN apt-get install -y --reinstall libpq-dev
+RUN pip3 install psycopg2
+RUN pip3 install sqlalchemy
+RUN pip3 install fastapi
+RUN pip3 install pydantic
+RUN pip3 install passlib
+RUN pip3 install typing
+RUN pip3 install uvicorn
+
+ENV DATABASE_URL="postgresql://pwmutypretbutp:e74306821a303bd574c10c1444dc22cd053edcc139d06b53fd5f457b1df725eb@ec2-34-199-15-136.compute-1.amazonaws.com:5432/df3a67v8n8bo9b"
+COPY main.py /app/
+COPY database_models /app/database_models
+COPY models /app/models
+
+CMD python3 main.py
--- main.py
+++ main.py
@@ -8,10 +8,15 @@ from sqlalchemy.orm import sessionmaker
 from sqlalchemy.ext.declarative import declarative_base
 import database_models.User as db_user
 from passlib.hash import pbkdf2_sha256
+import os
 
 
 app = FastAPI()
-engine = create_engine("postgres://pwmutypretbutp:e74306821a303bd574c10c1444dc22cd053edcc139d06b53fd5f457b1df725eb@ec2-34-199-15-136.compute-1.amazonaws.com:5432/df3a67v8n8bo9b")
+#engine = create_engine(process.env.DATABASE_URL)
+#engine = create_engine(os.environ['DATABASE_URL'])
+#engine = create_engine(DATABASE_URL)
+#engine = create_engine("${DATABASE_URL}")
+engine = create_engine(os.environ.get('DATABASE_URL'))
 session = sessionmaker(engine)()
 
 @app.get('/users/{username}', response_model=User)
