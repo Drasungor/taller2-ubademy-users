bf3fc0bee7e724634367210488fa43f8e9a09fd9
Agustin <aguscambiano@gmail.com>
2021-10-11 13:11:41 -0300

    Continued Users definition

--- models/user.py
+++ models/user.py
@@ -1,12 +1,30 @@
 from pydantic import BaseModel
 from typing import Optional
 from sqlalchemy import Column, String
+from sqlalchemy.ext.declarative import declarative_base
+from passlib.hash import pbkdf2_sha256
 
 
-CONST_EMAIL_LENGTH = 50 #TODO: VER EL TAMANIO MAXIMO DE LOS MAILS
-CONST_PASSWORD_LENGTH = 50
-CONST_SALT_LENGTH = CONST_PASSWORD_LENGTH
+CONST_EMAIL_LENGTH = 100 #TODO: VER EL TAMANIO MAXIMO DE LOS MAILS
+CONST_HASH_LENGTH = 250
+CONST_NAME_LENGTH = 40
 
+
+class User(Base):
+    __tablename__ = "users"
+
+    email = Column(String(CONST_EMAIL_LENGTH), primary_key = True)
+    hashed_password = Column(String(CONST_HASH_LENGTH))
+    name = Column(String(CONST_NAME_LENGTH))
+    #TODO: VER COMO METERIAMOS LO DEL USUARIO FEDERADO
+
+    def __init__(self, email, password):
+        self.email = email
+        self.hashed_password = pbkdf2_sha256.hash(password)
+
+
+
+"""
 
 fake_users_db = {
     "johndoe": {
@@ -19,19 +37,14 @@ fake_users_db = {
 }
 
 
-class User(Base):
-    __tablename__ = "users"
-
-    email = Column(Integer(String(CONST_EMAIL_LENGTH)))
-    hashed_password = Column(String(CONST_PASSWORD_LENGTH))
-    hash_salt = Column(String(CONST_SALT_LENGTH))
-    
 
 
-"""class User(BaseModel):
+class User(BaseModel):
     username: str
     hashed_password: str
     email: Optional[str] = None
     full_name: Optional[str] = None
-    disabled: Optional[bool] = None"""
+    disabled: Optional[bool] = None
+    
+"""
 
