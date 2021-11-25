dc036b2775bfefb4a1ed8f59f9e3667f45085a6d
Agustin <aguscambiano@gmail.com>
2021-11-25 20:30:51 -0300

    Could establish connection with local postgres database

--- database/database.py
+++ database/database.py
@@ -2,12 +2,15 @@ import os
 from sqlalchemy import create_engine
 from sqlalchemy.ext.declarative import declarative_base
 from sqlalchemy.orm import sessionmaker
+from time import sleep
 
+#TODO: VOLAR ESTO, METER EL COMANDO DE BASH DE GONZA PARA QUE SE SINCRONICEN BIEN LOS CONTAINERS DE DOCKER
+sleep(2)
 
 db_url = os.environ.get('DATABASE_URL', 'sqlite:///./test.db')
 # Some versions of sqlalchemy do not support postgres in the url, it has to be postgresql
 if db_url.find('postgresql') == -1:
-    db_url = db_url.replace('postgres', 'postgresql')
+    db_url = db_url.replace('postgres', 'postgresql', 1)
 
 engine_args = {"check_same_thread": False} if 'sqlite' in db_url else {}
 engine = create_engine(db_url, connect_args=engine_args)
