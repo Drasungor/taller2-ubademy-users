0ce56be2976a37fb8c8fbab588cba2f221a90878
Agustin <aguscambiano@gmail.com>
2021-11-27 17:41:04 -0300

    Added wait for database connection like the one used in the bash command

--- database/database.py
+++ database/database.py
@@ -4,16 +4,21 @@ from sqlalchemy.ext.declarative import declarative_base
 from sqlalchemy.orm import sessionmaker
 from time import sleep
 
-#TODO: VOLAR ESTO, METER EL COMANDO DE BASH DE GONZA PARA QUE SE SINCRONICEN BIEN LOS CONTAINERS DE DOCKER
-sleep(2)
-
 db_url = os.environ.get('DATABASE_URL', 'sqlite:///./test.db')
 # Some versions of sqlalchemy do not support postgres in the url, it has to be postgresql
 if db_url.find('postgresql') == -1:
     db_url = db_url.replace('postgres', 'postgresql', 1)
 
 engine_args = {"check_same_thread": False} if 'sqlite' in db_url else {}
-engine = create_engine(db_url, connect_args=engine_args)
+
+has_connected = False
+while not has_connected:
+    try:
+        engine = create_engine(db_url, connect_args=engine_args)
+    except Exception:
+        print(f"Failed to connect to database with url {db_url}, attempting to reconnect")
+    else:
+        has_connected = True
 
 session_args = {'bind': engine}
 if 'sqlite' in db_url:
