dbacc7e059056100968c42f8eddabe948c132250
Franco Scaccheri <fcassanello@gitlab.com>
2021-12-22 21:09:33 -0300

    Change test names

--- tests/test_newrelic_logger.py
+++ tests/test_newrelic_logger.py
@@ -10,21 +10,21 @@ def test_info_log(mock_newrelic_api):
 
 
 @patch('utils.newrelic_logger.requests.post')
-def test_info_log(mock_newrelic_api):
+def test_debug_log(mock_newrelic_api):
     mock_newrelic_api.return_value = MagicMock(status_code=200)
     logger = NewrelicLogger('newrelic-api-key')
     logger.debug('This is an DEBUG level log')
 
 
 @patch('utils.newrelic_logger.requests.post')
-def test_info_log(mock_newrelic_api):
+def test_warning_log(mock_newrelic_api):
     mock_newrelic_api.return_value = MagicMock(status_code=200)
     logger = NewrelicLogger('newrelic-api-key')
     logger.warning('This is an WARNING level log')
 
 
 @patch('utils.newrelic_logger.requests.post')
-def test_info_log(mock_newrelic_api):
+def test_error_log(mock_newrelic_api):
     mock_newrelic_api.return_value = MagicMock(status_code=200)
     logger = NewrelicLogger('newrelic-api-key')
     logger.error('This is an ERROR level log')
